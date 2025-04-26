14ec50042cfd5bb1045cbb17ae6d1a40c978d62a
zlongx <1251885400@qq.com>
2025-04-26 19:12:28 +0800

    更新

--- main.go
+++ main.go
@@ -36,21 +36,26 @@ func main() {
 
 	var secondMenuInfos []pg.MenuInfo
 	for i, barMenuInfo := range barMenuInfos {
+		lg.InfoToFileAndStdOut(fmt.Sprintf("bar正要处理 -> file=%s, menu=%s\n", barMenuInfo.Filename, barMenuInfo.MenuName))
 		err = pg.InitIndexMdFile(i, barMenuInfo)
 		if err != nil {
 			lg.ErrorToFileAndStdOutWithSleepSecond(fmt.Sprintf("%v", err), 3)
 			return
 		}
+		lg.InfoToFileAndStdOut(fmt.Sprintf("bar初始化完成 -> file=%s, menu=%s\n", barMenuInfo.Filename, barMenuInfo.MenuName))
 
 		secondMenuInfos, err = pg.InsertBarMenuPageData(browserHwnd, barMenuInfo, page)
 		if err != nil {
 			lg.ErrorToFileAndStdOutWithSleepSecond(fmt.Sprintf("%v", err), 3)
 			return
 		}
+		lg.InfoToFileAndStdOut(fmt.Sprintf("bar插入数据完成 -> file=%s, menu=%s\n", barMenuInfo.Filename, barMenuInfo.MenuName))
 
 		if len(secondMenuInfos) <= 0 {
 			continue
 		}
+
+		lg.InfoToFileAndStdOut(fmt.Sprintf("second处理二级菜单中 -> file=%s, menu=%s\n", barMenuInfo.Filename, barMenuInfo.MenuName))
 
 		for j, menuInfo := range secondMenuInfos {
 			err = pg.InitDetailPageMdFile(j, barMenuInfo, menuInfo)
@@ -58,10 +63,19 @@ func main() {
 				lg.ErrorToFileAndStdOutWithSleepSecond(fmt.Sprintf("%v", err), 3)
 				return
 			}
+			lg.InfoToFileAndStdOut(fmt.Sprintf("second初始化完成 -> file=%s, menu=%s secondfile=%s secondmenu=%s\n",
+				barMenuInfo.Filename, barMenuInfo.MenuName,
+				menuInfo.Filename, menuInfo.MenuName))
 
 			err = pg.InsertDetailPageData(browserHwnd, barMenuInfo, menuInfo, page)
-			lg.ErrorToFileAndStdOutWithSleepSecond(fmt.Sprintf("%v", err), 3)
-			return
+			if err != nil {
+				lg.ErrorToFileAndStdOutWithSleepSecond(fmt.Sprintf("%v", err), 3)
+				return
+			}
+			lg.InfoToFileAndStdOut(fmt.Sprintf("second插入数据完成 -> file=%s, menu=%s secondfile=%s secondmenu=%s\n",
+				barMenuInfo.Filename, barMenuInfo.MenuName,
+				menuInfo.Filename, menuInfo.MenuName))
+
 		}
 	}
 	time.Sleep(2000 * time.Second)
